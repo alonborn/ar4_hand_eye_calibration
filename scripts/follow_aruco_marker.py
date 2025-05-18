0320fc019b822fdee829b97d5a55e179b7690cce
Alon <alonborn@gmail.com>
2025-05-18 21:34:09 +0300

    Revert follow_aruco_marker.py to prevoious commit b11ca24fa59ad81166dc35890ae41d474533eddc

--- scripts/follow_aruco_marker.py
+++ scripts/follow_aruco_marker.py
@@ -14,24 +14,17 @@ from geometry_msgs.msg import Pose
 from ros2_aruco_interfaces.msg import ArucoMarkers
 from tf2_geometry_msgs import do_transform_pose
 from pymoveit2 import MoveIt2
-from my_robot_interfaces.srv import MoveToPose  # Import the custom service type
-import time
-import debugpy
-import transforms3d
-from std_srvs.srv import SetBool
-import asyncio
+
 
 class ArucoMarkerFollower(Node):
 
     def __init__(self):
         super().__init__("aruco_marker_follower")
         self.logger = self.get_logger()
-        self.processing_done = asyncio.Event()
-        self.processing_done.set()  # Initially not processing
+
         self.arm_joint_names = [
             "joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6"
         ]
-        self.follower_enabled = True 
         self.moveit2 = MoveIt2(
             node=self,
             joint_names=self.arm_joint_names,
@@ -43,48 +36,26 @@ class ArucoMarkerFollower(Node):
         self.moveit2.planner_id = "RRTConnectkConfigDefault"
         self.moveit2.max_velocity = 1.0
         self.moveit2.max_acceleration = 1.0
-        self.move_completed_successfully = False
-        self.cb_group = ReentrantCallbackGroup()
-        self.cb_group_aruco_marker = ReentrantCallbackGroup()
 
-        self.arm_is_available = True
         # ID of the aruco marker mounted on the robot
         self.marker_id = self.declare_parameter(
             "marker_id", 1).get_parameter_value().integer_value
 
-        self.subscription = self.create_subscription(ArucoMarkers,"/aruco_markers",self.handle_aruco_markers,1,callback_group=self.cb_group_aruco_marker)
-        self.pose_pub = self.create_publisher(PoseStamped, "/cal_marker_pose",1)
-        self.enable_service = self.create_service(
-            SetBool,
-            "set_aruco_follower_enabled",
-            self.handle_enable_service,
-            callback_group=self.cb_group,
-        )
-        self.target_pose_pub = self.create_publisher(PoseStamped, "/follow_aruco_target_pose", 1)
+        self.subscription = self.create_subscription(ArucoMarkers,
+                                                     "/aruco_markers",
+                                                     self.handle_aruco_markers,
+                                                     1)
+        self.pose_pub = self.create_publisher(PoseStamped, "/cal_marker_pose",
+                                              1)
+
+        self.target_pose_pub = self.create_publisher(
+            PoseStamped, "/follow_aruco_target_pose", 1)
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
         self._prev_marker_pose = None
 
-        # Timer: callback every 2.0 seconds
-        # self.timer_callback_group = ReentrantCallbackGroup()
-        # self.timer = self.create_timer(2.0, self.timer_callback,callback_group=self.timer_callback_group)
-
-    # def timer_callback(self):
-    #     self.get_logger().info('ROS loop is alive!')
-
-    def handle_enable_service(self, request, response):
-        self.follower_enabled = request.data
-        status = "enabled" if self.follower_enabled else "disabled"
-        self.logger.info(f"Aruco follower {status}")
-        response.success = True
-        response.message = f"Aruco follower {status}"
-        return response
-
-    def _process_marker(self,msg):
-        self.get_logger().info("Received aruco markers")
+    def handle_aruco_markers(self, msg: ArucoMarkers):
         cal_marker_pose = None
-        if (not self.arm_is_available):
-            return
         for i, marker_id in enumerate(msg.marker_ids):
             if marker_id == self.marker_id:
                 cal_marker_pose = msg.poses[i]
@@ -98,11 +69,11 @@ class ArucoMarkerFollower(Node):
         # only start following if the marker pose has changed by at least 2cm
         if self._prev_marker_pose is not None:
             if ((cal_marker_pose.position.x -
-                self._prev_marker_pose.position.x)**2 +
+                 self._prev_marker_pose.position.x)**2 +
                 (cal_marker_pose.position.y -
-                self._prev_marker_pose.position.y)**2 +
+                 self._prev_marker_pose.position.y)**2 +
                 (cal_marker_pose.position.z -
-                self._prev_marker_pose.position.z)**2 > 0.02**2):
+                 self._prev_marker_pose.position.z)**2 > 0.02**2):
                 self._prev_marker_pose = cal_marker_pose
                 return
 
@@ -117,39 +88,11 @@ class ArucoMarkerFollower(Node):
             self.logger.error(f"Error transforming pose: {e}")
             return
 
-        # first flip the pose up side down
-
-        quat = [
-            transformed_pose.orientation.w,
-            transformed_pose.orientation.x,
-            transformed_pose.orientation.y,
-            transformed_pose.orientation.z,
-        ]
-        x_180_deg_quat = [0, 1, 0, 0]
-        flipped_quat = transforms3d.quaternions.qmult(quat, x_180_deg_quat)
-        transformed_pose.orientation.w = flipped_quat[0]
-        transformed_pose.orientation.x = flipped_quat[1]
-        transformed_pose.orientation.y = flipped_quat[2]
-        transformed_pose.orientation.z = flipped_quat[3]
-
-        transformed_pose.position.z += 0.09        
-
         self.logger.info(f"Following marker at pose: {transformed_pose}")
         self.move_to(transformed_pose)
-    
-    async def handle_aruco_markers(self, msg: ArucoMarkers):
-        if not self.follower_enabled or not self.processing_done.is_set():
-            return
-        self.processing_done.clear()
-        
-        self.is_processing_marker = True
-        try:
-            await self._process_marker(msg)
-        finally:
-            self.processing_done.set()
-    
+
     def _transform_pose(self, pose: Pose, source_frame,
-                        target_frame: str) -> Pose: 
+                        target_frame: str) -> Pose:
         # Get the transform from source frame to target frame
         transform = self.tf_buffer.lookup_transform(target_frame, source_frame,
                                                     Time())
@@ -174,21 +117,15 @@ class ArucoMarkerFollower(Node):
         pose_goal = PoseStamped()
         pose_goal.header.frame_id = "base_link"
         pose_goal.pose = msg
-        
+
         self.moveit2.move_to_pose(pose=pose_goal)
         self.moveit2.wait_until_executed()
 
 
 def main():
-
-    debugpy.listen(("0.0.0.0", 5678))
-    print("Waiting for debugger to attach...")
-    debugpy.wait_for_client()  # Uncomment this if you want to pause execution until the debugger attaches
-    print("debugger attached")
-        
     rclpy.init()
     node = ArucoMarkerFollower()
-    executor = MultiThreadedExecutor(7)
+    executor = MultiThreadedExecutor(4)
     executor.add_node(node)
     try:
         executor.spin()
