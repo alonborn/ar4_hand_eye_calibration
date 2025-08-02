de36a3cd3338f130f6175f2139c8d18fe29810cd
Alon <alonborn@gmail.com>
2025-08-02 15:30:47 +0300

    remove debugging code from main function in follow_aruco_marker script

--- scripts/follow_aruco_marker.py
+++ scripts/follow_aruco_marker.py
@@ -284,10 +284,10 @@ class ArucoMarkerFollower(Node):
     #     future.add_done_callback(self.handle_response)
 
 def main():
-    debugpy.listen(("0.0.0.0", 5678))
-    print("Waiting for debugger to attach...")
-    debugpy.wait_for_client()  # Uncomment this if you want to pause execution until the debugger attaches
-    print("debugger attached")
+    # debugpy.listen(("0.0.0.0", 5678))
+    # print("Waiting for debugger to attach...")
+    # debugpy.wait_for_client()  # Uncomment this if you want to pause execution until the debugger attaches
+    # print("debugger attached")
 
     rclpy.init()
     node = ArucoMarkerFollower()
